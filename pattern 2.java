0e1529551458fb27d4136b311636b4fe41d5dd40
GeethaBhavani28 <84717547+GeethaBhavani28@users.noreply.github.com>
2021-06-18 11:48:26 +0530

    Add files via upload

--- pattern 2.java
+++ pattern 2.java
@@ -1,18 +1,12 @@
 package Patterns;
-public class File2 {
+public class File1 {
 	public static void main(String[] args) {
-		int x=1;
-		for(int i = 1; i <= 5; i++) // 5 rows -> 1 2 3 4 5 
+		for(int i = 1; i<=5;i++) // 5 rows - 1,2,3,4,5
 		{
-			for( int j = 5; j > i; j--) // 4 3 2 1 0 spaces
-			{
-				System.out.print(" ");
-			}
-			for(int n=1; n <= x;n++) // 1 3 5 7 9 times
-			{
-				System.out.print((char)(x+64));				
-			}
-			x = x+2; // x= 3,5,7,9
-			System.out.println();
+		for (int j =1; j<=5; j++) // 5 times
+		{
+			System.out.print(i);
 		}
-} }
+		System.out.println();
+		}
+	} }

781949bcd292f20d52f24b6599467c0ff5a5c08e
GeethaBhavani28 <84717547+GeethaBhavani28@users.noreply.github.com>
2021-06-18 11:46:24 +0530

    Add files via upload

---
+++ pattern 1.java
@@ -0,0 +1,11 @@
+package Patterns;
+public class File1 {
+	public static void main(String[] args) {
+		for(int i = 1; i<=5;i++) // 5 rows
+		{
+		for (int j =1; j<=5; j++) // 5 cols
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+		}

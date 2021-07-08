f8907c2192d488f582be5f43d139353496e01738
GeethaBhavani28 <84717547+GeethaBhavani28@users.noreply.github.com>
2021-07-08 21:02:27 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        // write code here
+
+        for(int i = 1; i<=10; i++)
+        {
+            System.out.println(N+" x "+i+" = "+ N*i);
+        }
+
+        bufferedReader.close();
+    }
+}

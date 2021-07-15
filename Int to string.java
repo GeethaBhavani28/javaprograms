e3b3639857c7bfb74a12ce24822f16fe95b0d977
GeethaBhavani28 <84717547+GeethaBhavani28@users.noreply.github.com>
2021-07-15 13:05:27 +0530

    Add files via upload

---
+++ Int to string.java
@@ -0,0 +1,24 @@
+public class Solution {
+ public static void main(String[] args) {
+
+  DoNotTerminate.forbidExit();
+
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+   //String s=???; Complete this line below
+
+   //Write your code here
+String s = Integer.toString(n);
+   
+   if (n == Integer.parseInt(s)) {
+    System.out.println("Good job");
+   } else {
+    System.out.println("Wrong answer.");
+   }
+  } catch (DoNotTerminate.ExitTrappedException e) {
+   System.out.println("Unsuccessful Termination!!");
+  }
+ }
+}
